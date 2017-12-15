ad8769ba029de2870518be223834200fc353c9db
Akshat Jain <jainakshat9595@gmail.com>
2017-12-15 21:38:20 +0530

    Images uploaded

--- app/src/main/java/com/wishadesign/app/skochvoting/fragment/SlideFragment.java
+++ app/src/main/java/com/wishadesign/app/skochvoting/fragment/SlideFragment.java
@@ -7,9 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.wishadesign.app.skochvoting.R;
+import com.wishadesign.app.skochvoting.activity.MainActivity;
 
 public class SlideFragment extends Fragment {
 
@@ -40,8 +44,36 @@ public class SlideFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_slide, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.img_placeholder);
+        ImageView imageView_login = (ImageView) rootView.findViewById(R.id.loginbtn);
+        imageView_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity)getActivity()).getViewPager().setCurrentItem(6);
+            }
+        });
+        switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+            case 1:
+                Glide.with(getContext()).load(R.drawable.screen1).apply(new RequestOptions().centerCrop()).into(imageView);
+                imageView_login.setVisibility(View.GONE);
+                break;
+            case 2:
+                Glide.with(getContext()).load(R.drawable.screen2).apply(new RequestOptions().centerCrop()).into(imageView);
+                break;
+            case 3:
+                Glide.with(getContext()).load(R.drawable.screen3).apply(new RequestOptions().centerCrop()).into(imageView);
+                imageView_login.setVisibility(View.GONE);
+                break;
+            case 4:
+                Glide.with(getContext()).load(R.drawable.screen4).apply(new RequestOptions().centerCrop()).into(imageView);
+                break;
+            case 5:
+                Glide.with(getContext()).load(R.drawable.screen5).apply(new RequestOptions().centerCrop()).into(imageView);
+                break;
+            case 6:
+                Glide.with(getContext()).load(R.drawable.screen6).apply(new RequestOptions().centerCrop()).into(imageView);
+                break;
+        }
         return rootView;
     }
 
